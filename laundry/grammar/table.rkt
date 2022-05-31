d01aa802bb07b11b68949fdcb62d1cbd4814b8ac
Tom Gillespie <tgbugs@gmail.com>
2022-05-31 11:19:38 -0700

    table grammar cleanup and documentation

--- laundry/grammar/table.rkt
+++ laundry/grammar/table.rkt
@@ -1,39 +1,22 @@
 #lang brag
 
+; conceptually we would like to be able to use the following definition for table
+; table : ( /NEWLINE wsnn1-n? ( table-row | table-row-rule ) )+ /NEWLINE?
+; however there is hidden quadratic behavior because there are optional elements inside table-row
+
 table : /NEWLINE wsnn1-n? ( table-row | table-row-rule | table-row-before-rule table-row-rule )+
 
-;( ( /NEWLINE | /PIPE-FINAL ) wsnn1-n? ( table-row | table-row-rule )? )+
-;table : ( /NEWLINE wsnn1-n? ( table-row | table-row-rule ) )
-      ;| ( /NEWLINE wsnn1-n? ( table-row | table-row-rule ) ) ( ( /NEWLINE | /PIPE-FINAL ) wsnn1-n? ( table-row | table-row-rule )? )+
-
-;-table : ( /NEWLINE wsnn1-n? table-row | table-row-rule )+ /NEWLINE?
-
-;table-row-degenerate : table-cell-degenerate
+; we special case this to avoid using ? in the definition of table-row
 table-cell-degenerate : /PIPE-FINAL
 
-table-row-rule : TABLE-ROW-RULE /NEWLINE? ; FIXME multiple table-row-rules in a row will break this
-table-row-before-rule : table-cell+
-table-row : table-cell+ ( /PIPE-FINAL | /NEWLINE ) ; XXX ()? or /NEWLINE? absolutely tanks performance, otherwise this is ALMOST linear
-          ;| table-cell+  ; this also tanks performance
+table-row : table-cell+ ( /PIPE-FINAL | /NEWLINE ) ; ? or table-cell+ alone induces the quadraticness
           | table-cell-degenerate
-          ;| table-cell+ /PIPE ; I don't think this variant works due to ambiguity
-;table-cell : /PIPE wsnn1-n? ( REST | ESC-PIPE | BS | wsnn1-n )* wsnn1-n? ; FIXME @wsnn is ambiguous so have to strip ?
+
+table-row-before-rule : table-cell+ ; the table row rule token is unavoidably new line first
+
+table-row-rule : TABLE-ROW-RULE /NEWLINE? ; /NEWLINE? is safe, needed for multiple rule rows in a ... row (lol)
 
 table-cell : /PIPE
            | /PIPE ( REST | ESC-PIPE | BS | wsnn1-n )+
-;table-row : table-cell-first table-cell-rest* /PIPE?
-; XXX divergence due to being able to escape pipes
-;@table-cell-first : /PIPE ( ( /wsnn+ | not-pbhn ) ( not-pipe-bs-newline | ESC-PIPE | BS )* )? /wsnn*
-;@table-cell-rest : /PIPE /wsnn* ( not-pipe-bs-newline | ESC-PIPE | BS )* /wsnn*
-;table-row-rule : /PIPE /HYPHEN /not-newline* ; everything else gets wiped XXX but careful with roundtrips?
 
 @wsnn1-n: WSNN1-N+
-
-;wsnn: space | tab
-;@not-pbhn : REST | space | tab
-;@not-pipe-bs-newline : REST | space | tab | HYPHEN
-;not-newline : REST | space | tab | HYPHEN | PIPE | BS
-
-; have to have explicit nodes for these since they aren't lexemes
-;tab : TAB
-;space : SPACE | SPACES

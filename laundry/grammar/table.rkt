2ca8cb38fcfdd1ddc635f03a2839e2ea71d63b7a
Tom Gillespie <tgbugs@gmail.com>
2022-05-31 21:42:05 -0700

    table grammar, newline first strikes back
    so after many iterations, as far as I can tell, there is no way to use
    a newline first parser for the tables IF you are going to use racket
    parser tools because behind the scenes you always fall down a
    quadratic hole
    
    this commit preserves a record of the so-far-best attempt at a grammar
    for parsing tables in nlf manner

--- laundry/grammar/table.rkt
+++ laundry/grammar/table.rkt
@@ -4,19 +4,35 @@
 ; table : ( /NEWLINE wsnn1-n? ( table-row | table-row-rule ) )+ /NEWLINE?
 ; however there is hidden quadratic behavior because there are optional elements inside table-row
 
-table : /NEWLINE ( wsnn1-n? ( table-row | table-row-rule | table-row-before-rule table-row-rule ) )+
+table : table-nlf
+
+;table : table-nll
+
+table-nll : /NEWLINE ( table-row-rule | table-row-variants-nll )
+
+@table-row-variants-nll : ( table-row-degenerate | table-row-open | table-row )
+
+table-nlf : ( ( table-row-variants
+              | table-row-rule
+              ) )+ /NEWLINE?
+
+;@start : /NEWLINE /wsnn1-n?
+@table-row-variants : ( /NEWLINE | /NLWS ) ( table-row-degenerate | table-row-open | table-row )
 
 ; we special case this to avoid using ? in the definition of table-row
-table-cell-degenerate : /PIPE-FINAL
+table-row-degenerate : table-cell-degenerate
+table-cell-degenerate : /PIPE
 
-table-row : table-cell+ ( /PIPE-FINAL | /NEWLINE ) ; ? or table-cell+ alone induces the quadraticness
-          | table-cell-degenerate
+table-row : table-cell+ /PIPE ; ? or table-cell+ alone induces the quadraticness
+          ; | table-cell-degenerate
 
-table-row-before-rule : table-cell+ ; the table row rule token is unavoidably new line first
+table-row-open : table-cell+
 
-table-row-rule : TABLE-ROW-RULE /NEWLINE? ; /NEWLINE? is safe, needed for multiple rule rows in a ... row (lol)
+;table-row-before-rule : table-cell+ ; the table row rule token is unavoidably new line first
 
-table-cell : /PIPE
-           | /PIPE ( REST | ESC-PIPE | BS | wsnn1-n )+
+table-row-rule : TABLE-ROW-RULE ; /NEWLINE? ; /NEWLINE? is safe, needed for multiple rule rows in a ... row (lol)
+
+table-cell : /PIPE ( REST | ESC-PIPE | BS | wsnn1-n )+
+           | /PIPE
 
 @wsnn1-n: WSNN1-N+
